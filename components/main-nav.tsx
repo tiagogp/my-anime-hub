89364af83b2437b93049960a1a5f7187f4991c4d
Tiago Guimarães Pinto <tguimaraespinto@gmail.com>
2024-01-26 15:05:53 -0400

    refactor: adjustment

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,8 +3,6 @@
 import * as React from "react"
 import Link from "next/link"
 
-import { NavItem } from "@/types/nav"
-import { cn } from "@/lib/utils"
 import { Icons } from "@/components/icons"
 
 export function MainNav() {
